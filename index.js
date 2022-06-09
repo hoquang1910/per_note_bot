d9606072c0cafcf3d605b4fa8c289955b251d2e9
Ho Quang <19520875@gm.uit.edu.vn>
2022-06-09 23:17:12 +0700

    make it better

--- index.js
+++ index.js
@@ -69,16 +69,16 @@ const dialogflowFulfillment = (request,response) => {
        function handleReadFromMySQL(agent){
          const name_disease = agent.parameters['any'];
          const disease = agent.parameters['disease'];
-         const kind = ChangeValue(disease);
+         const st = ChangeValue(disease);
         return connectToDatabase()
         .then(connection => {
-          return queryDatabase(connection,'name_disease')
+          return queryDatabase(connection,st)
           .then(result => {
             console.log(result);
             result.map(name =>{
               if(name_disease === name.name_disease)
               {
-                agent.add(`${disease} của bệnh là : ${name.kind}`);
+                agent.add(`${disease} của bệnh là : ${name.name_disease}`);
               }
             });
             connection.end();
