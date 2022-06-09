4e2ec23aab3f38261c61138a7b621e9e4a0c7423
Ho Quang <19520875@gm.uit.edu.vn>
2022-06-09 23:49:32 +0700

    make it better

--- index.js
+++ index.js
@@ -75,24 +75,23 @@ const dialogflowFulfillment = (request,response) => {
           return queryDatabase(connection,st,name_disease)
           .then(result => {
             console.log(result);
-            // if(true)
-            // {
-            //     if(disease === "Thông tin")
-            //         agent.add(`${disease} của bệnh là : ${result.introduce}`);
-            //     if(disease === "Nguyên nhân")
-            //         agent.add(`${disease} của bệnh là : ${result.reason}`);
-            //     if(disease === "Triệu chứng")
-            //         agent.add(`${disease} của bệnh là : ${result.symptom}`);
-            //     if(disease === "Ai")
-            //         agent.add(`${disease} của bệnh là : ${result.objects}`);
-            //     if(disease === "Phòng ngừa")
-            //         agent.add(`${disease} của bệnh là : ${result.prevent}`);
-            //     if(disease === "Chẩn đoán")
-            //         agent.add(`${disease} của bệnh là : ${result.diagnose}`);
-            //     if(disease === "Giải pháp")
-            //         agent.add(`${disease} của bệnh là : ${result.solution}`);
-            // }
-            agent.add(`${result[0].reason}`)
+            if(true)
+            {
+                if(disease === "Thông tin")
+                    agent.add(`${disease} của bệnh là : ${result[0].introduce}`);
+                if(disease === "Nguyên nhân")
+                    agent.add(`${disease} của bệnh là : ${result[0].reason}`);
+                if(disease === "Triệu chứng")
+                    agent.add(`${disease} của bệnh là : ${result[0].symptom}`);
+                if(disease === "Ai")
+                    agent.add(`${disease} của bệnh là : ${result[0].objects}`);
+                if(disease === "Phòng ngừa")
+                    agent.add(`${disease} của bệnh là : ${result[0].prevent}`);
+                if(disease === "Chẩn đoán")
+                    agent.add(`${disease} của bệnh là : ${result[0].diagnose}`);
+                if(disease === "Giải pháp")
+                    agent.add(`${disease} của bệnh là : ${result[0].solution}`);
+            }
             connection.end();
           });
         });
