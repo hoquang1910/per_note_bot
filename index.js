7ae6bd8274ea824a9128422ecf6248329cf75ef8
Ho Quang <19520875@gm.uit.edu.vn>
2022-06-09 22:57:54 +0700

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {WebhookClient} = require('dialogflow-fulfillment')
+const sql = require('mysql');
 const { request } = require('http')
 const { response } = require('express')
 
@@ -19,10 +20,67 @@ app.listen(port,()=>{
 const dialogflowFulfillment = (request,response) => {
     const agent = new WebhookClient({request,response})
 
-    function a(){
-        agent.add('Nguyên nhân của AIDS');
-    }
+    function connectToDatabase(){
+        const connection = sql.createConnection({
+          host     : 'sql6.freemysqlhosting.net',
+          user     : 'sql6498706',
+          password : 'ztQBhxg8X1',
+          database : 'sql6498706'
+        });
+        return new Promise((resolve,reject) => {
+           connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+           resolve(connection);
+        }).catch((err) => {
+            console.error('Failed to load.', err);
+            return Promise.reject(err);
+          });
+      }
+      
+      function queryDatabase(connection){
+        return new Promise((resolve, reject) => {
+          connection.query('Select * from disease', (error, results, fields) => {
+            resolve(results);
+            
+          });
+        });
+      }
+      
+    //   function ChangeValue(value){
+    //     if(value === "Thông tin")
+    //       return "introduce";
+    //     if(value === "Nguyên nhân")
+    //       return "reason";
+    //     if(value === "Triệu chứng")
+    //       return "symptom";
+    //     if(value === "Ai")
+    //       return "objects";
+    //     if(value === "Phòng ngừa")
+    //       return "prevent";
+    //     if(value === "Chẩn đoán")
+    //       return "diagnose";
+    //     if(value === "Giải pháp")
+    //       return "solution";
+    //   }
+      
+      
+       function handleReadFromMySQL(agent){
+        //  const name_disease = agent.parameter.any;
+        //  const disease = agent.parameter.disease;
+        //  const kind = ChangeValue(disease);
+        return connectToDatabase()
+        .then(connection => {
+          return queryDatabase(connection)
+          .then(result => {
+            console.log(result);
+            agent.add(` của bệnh là : ${result[0].name_disease}`);
+            connection.end();
+          });
+        });
+      }
     let intentMap = new Map();
-    intentMap.set('getDataFromSQL', a);
+    intentMap.set('getDataFromSQL', handleReadFromMySQL);
     agent.handleRequest(intentMap);
 }
